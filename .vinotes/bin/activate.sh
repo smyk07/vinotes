e5275d70ae67e0860c4a27bc1f8b2217b21ccd7c
smyk07 <samyak.bambole07@gmail.com>
2024-07-12 20:24:38 +0530

    alias cd command to avoid commands breaking

--- .vinotes/bin/activate.sh
+++ .vinotes/bin/activate.sh
@@ -5,7 +5,7 @@ NEW_PS1="(vinotes) ${PS1}"
 # update bash prompt
 export PS1=${NEW_PS1}
 
-# handling path
+# handling path and venv variables
 export VIRTUAL_ENV="$PWD/.vinotes/"
 OLD_PATH="$PATH"
 NEW_PATH="$VIRTUAL_ENV/bin:$PATH"
@@ -13,9 +13,23 @@ NEW_PATH="$VIRTUAL_ENV/bin:$PATH"
 # update path
 export PATH=${NEW_PATH}
 
+# alias cd function
+cd() {
+  if [[ "$1" = '--force' ]] || [[ "$1" = '-f' ]]; then
+    shift
+    command cd "${@}"
+    return
+  fi
+  command echo "Vinotes requires you to stay in your vault directory always."
+  command echo "(use -f or --force to use the command but note that doing so, Vinotes functionality may break.)"
+  command echo ""
+}
+
 # deactivate function
 deactivate() {
-  export PATH=${OLD_PATH}
   export PS1=${OLD_PS1}
+  export PATH=${OLD_PATH}
+  unset cd
+  unset deactivate
   echo "Bye..."
 }
