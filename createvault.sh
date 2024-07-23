3d20a1784e77b6ae348112e225f9c75174db49a7
smyk07 <samyak.bambole07@gmail.com>
2024-07-23 20:06:27 +0530

    some changes for testing [dev]

--- createvault.sh
+++ createvault.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-echo "Cloning repo into ."
+echo "Cloning repo..."
+echo ""
 command git clone https://github.com/smyk07/vinotes.git .
 
 echo "Repo Cloned"
 command rm -rf .git todo.md README.md
 
-echo "Get started by entering the command:"
+echo ""
+echo "▷ Get started by entering the command:"
 echo "    source .vinotes/bin/activate.sh"
 echo ""
-echo "You can alias the above command in your ~/.bashrc to make it easier to enter into the vault."
+echo "▷ You can alias the above command in your ~/.bashrc to make it easier to enter into the vault."
 echo ""
-echo "Make sure to edit your config by entering the command:"
+echo "▷ Make sure to edit your config by entering the command:"
 echo "    vinotes conf"
 echo ""
-echo "This default vault comes with a Zettlekasten note-taking system and a daily-note creation feature"
+echo "▷ This default vault comes with a Zettlekasten note-taking system and a daily-note creation feature"
 echo ""
-echo "For customization, view the documentation"
+echo "▷ For customization, view the documentation"
